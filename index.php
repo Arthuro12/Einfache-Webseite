1bb7b268d1afef5395510468b778886aa24e7138
Arthuro12 <77574113+Arthuro12@users.noreply.github.com>
2021-01-22 20:49:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+   <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Alcastra</title>
+        <link rel="stylesheet" href="style.css" />        
+   </head>
+   
+   <body>
+       <!-- Das ist mein Header -->
+       <header class="headerein">
+            <div id="haupttitel">
+                <div class="logoTop">
+                <a class="logo" href="#">AlcasTra-Gruppe</a>
+                </div>
+                <h4 id="slogan">Immer für Sie da</h4>
+            </div>         
+
+            <nav>
+               <ul>
+                    <li><a href="#">Start</a></li>
+                    <li><a href="#">Wer sind wir?</a></li>
+                    <li><a href="#">Was wir machen</a></li>
+                    <li><a href="#">Karriere</a></li>
+               </ul>
+            </nav>
+        </header>
+       
+       <div id="mitte">
+            <!-- Banner -->
+            <div id="Banner">
+                <div class="BannerBeschreibung">              
+                    <h3>Uns und Sie</h3>
+                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                </div>
+            </div>
+        
+           <!-- Das ist mein Section -->
+           <section>
+                <aside class="kennelernen">
+                    <h1>Wir Sind</h1>
+                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                      Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. 
+                      Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                      Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. 
+                      Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+                    </p><br />
+                    <h4>HTML Ipsum Presents</h4>
+                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                       Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. 
+                       Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. 
+                       Mauris placerat eleifend leo.</p>
+                    <a class="boutonVert" href="#">Mehr über uns?</a>
+                </aside>
+                <article class="partner" style="margin-left: 2rem; margin-right: 0;">
+                    <h2>Unser Partner</h2>
+                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                    Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. 
+                    Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                    <img src="Bilder/partnerein.webp" alt="Partner1" title="Partner1" heigh=150px width="400px" />
+                    <a class="buttonPartner" href="#" style="position: relative; bottom: 100px; right: 30px;">Mehr erfahren?</a>
+                </article>                         
+            </section>
+
+           <!-- Das ist mein Footer -->
+            <footer>
+                <div class="kontakt">
+                    <div class="tel">
+                       <h3>Ihr Ansprechpartner für alle Fragen</h3>
+                       <p>Thomas Lange</p>
+                       <a href="tel:+49 1513 4454455" style="display: block;"><span>&#9742;</span>+49 1513 4454455</a>
+                    </div>                             
+                   <a class="mail" href="mailto:arthurfoadjo@yahoo.com" style="display: block;">E-Mail</a>
+                </div>
+                <div class="cookie">
+                    <p>Diese Website kann Cookies nutzen, um ein gutes Nutzererlebnis zu gewährleisten. Weitere Informationen finden Sie in unserem <a class="daten" href="#">Datenschutz</a></p>
+                    <div class="mesdeuxboutons">
+                        <a class="buttoncookieeinstellen" href="#">Cookie-Einstellungen</a>
+                        <a class="buttoncookieakzeptieren" href="#">Cookie-Akzeptieren</a>
+                    </div>
+                </div> 
+            </footer>     
+        </div>
+   </body>
+</html>
